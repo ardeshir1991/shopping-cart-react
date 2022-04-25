5bbf2eefb9fc1e05257c6cea9c0ee465eff28431
ardeshir <ardeshiref1991@gmail.com>
2022-04-25 23:42:02 +0430

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import './app.scss';
+
+const App = () => {
+    return ( 
+        <div className="grid-container">
+            <header>
+                <a href="/">React Shopping Cart</a>
+            </header>
+            <main>Products list</main>
+            <footer>All rights reserved</footer>
+        </div>
+     );
+}
+ 
+export default App;
